bda1da8e6111327b3932793b9f7221a35a4c722f
muhyiii <muhyiii47@gmail.com>
2023-01-27 17:09:05 +0700

    tes

--- src/PAGES/Home.jsx
+++ src/PAGES/Home.jsx
@@ -1,15 +1,41 @@
 import { motion } from "framer-motion";
 import React from "react";
 import { BiSearch } from "react-icons/bi";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 import Product from "./Product";
 
 const Home = () => {
+  const [isSearching, setIsSearching] = React.useState(false);
+  const [isAvailable, setIsAvailable] = React.useState(false);
   return (
     <div className="text-gray-400">
       <div className="w-full  bg-black text-white py-3 px-40 flex justify-between items-center">
         <h1 className="gugi text-2xl">ZEENAH</h1>
-        <BiSearch size={25} />
+        <div className="flex w-1/5  justify-end space-x-5">
+          <div className=" ">
+            {isSearching ? (
+              <div className="flex border-b w-full justify-between  ">
+                <input
+                  type="text"
+                  className="bg-transparent outline-none w-full  text-base"
+                />
+                <BiSearch
+                  className="cursor-pointer"
+                  size={25}
+                  onClick={() => setIsSearching(!isSearching)}
+                />
+              </div>
+            ) : (
+              <BiSearch
+                className="cursor-pointer"
+                size={25}
+                onClick={() => setIsSearching(!isSearching)}
+              />
+            )}
+          </div>
+          <AiOutlineShoppingCart size={25} />
+        </div>
       </div>
       <div className="w-full bg-black relative grid grid-cols-12  px-10 items-center justify-center">
         <div className="background w-[1000px] h-[600px] m-auto mx-20" />
@@ -20,13 +46,12 @@ const Home = () => {
         >
           <motion.p
             initial={{ x: -100, opacity: 0 }}
-            animate={{ x: 0, opacity: [0, 1,0.5,1] }}
+            animate={{ x: 0, opacity: [0, 1, 0.5, 1] }}
             transition={{
               delay: 0.5,
               bounce: 0.1,
               type: "spring",
               duration: 1,
-              
             }}
             className="text-6xl font-semibold inter mb-3 "
           >
@@ -43,13 +68,31 @@ const Home = () => {
       </div>
       <div className="px-40 mt-5">
         <div className="flex space-x-3">
-          <p className="border-b-2 pb-1 text-black border-black font-semibold">
+          <motion.button
+            whileTap={{ scale: 0.9 }}
+            whileHover={{ scale: 1.1 }}
+            onClick={() => setIsAvailable(!isAvailable)}
+            className={` ${
+              !isAvailable &&
+              `border-b-2 pb-1 text-black border-black font-semibold`
+            } cursor-pointer `}
+          >
             PRODUCT
-          </p>
-          <p>AVAILABLE STOCK</p>
+          </motion.button>
+          <motion.button
+            whileTap={{ scale: 0.9 }}
+            whileHover={{ scale: 1.1 }}
+            onClick={() => setIsAvailable(!isAvailable)}
+            className={` ${
+              isAvailable &&
+              `border-b-2 pb-1 text-black border-black font-semibold`
+            } cursor-pointer `}
+          >
+            AVAILABLE STOCK
+          </motion.button>
         </div>
       </div>
-      <Product />
+      <Product isAvailable={isAvailable} setIsAvailable={setIsAvailable} />
     </div>
   );
 };
--- src/PAGES/Product.jsx
+++ src/PAGES/Product.jsx
@@ -1,84 +1,153 @@
 import React from "react";
 import product from "../ASSETS/product.jpg";
 import { BsFillCartPlusFill } from "react-icons/bs";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import data from "../ASSETS/tes.json";
 
-const Product = () => {
-  let e = [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
-
-  const textAnimate = {
-    offscreen: { y: 100, opacity: 0 },
-    onscreen: {
-      y: 0,
-      opacity: 1,
-      transition: { type: "spring", bounce: 0.4, duration: 1 },
-    },
-  };
+const Product = ({ isAvailable, setIsAvailable }) => {
   return (
     <div className="grid grid-cols-12 gap-10 px-40 mt-10 text-black">
-      {e.map((e) => (
-        <motion.div
-          layout
-          initial={{ opacity: 0, y: -100 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            delay: 2,
-            duration: 1.5,
-            type: "spring",
-            bounce: 0.5,
-            opacity: { ease: "linear" },
-            layout: { duration: 0.3 },
-          }}
-          className="bg-white p-3 shadow-xl relative col-span-3 text-left capitalize space-y-2 rounded-sm "
-        >
-          <div className="relative">
-            <motion.img
-              initial={{ opacity: 0, x: -100 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{
-                delay: 3,
-                duration: 1,
-                bounce: 0.3,
-                type: "spring",
-              }}
-              src={product}
-              className="img z-10"
-              alt=""
-            />
-          </div>
-          <div>
-            <motion.p
-              initial={{ opacity: 0, y: 100 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{
-                delay: 3,
-                duration: 1,
-                bounce: 0.3,
-                type: "spring",
-              }}
-              className="inter text-lg"
-            >
-              Jubah Zeenah
-            </motion.p>
-            <motion.p
-              initial={{ opacity: 0, y: 100 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{
-                delay: 3.2,
-                duration: 1,
-                bounce: 0.3,
-                type: "spring",
-              }}
-              className="text-sm"
-            >
-              Rp 560.000,00
-            </motion.p>
-          </div>
-          <button className="text-center py-2 w-full bg-yellow-500 inter flex items-center justify-center space-x-5">
-            <p> Add To Cart</p> <BsFillCartPlusFill />
-          </button>
-        </motion.div>
-      ))}
+      <AnimatePresence>
+        {isAvailable
+          ? data
+              .filter((e) => e.stok > 0)
+              .map((e, i) => (
+                <motion.div
+                  key={i}
+                  layout
+                  initial={{ opacity: 0, y: -100, scale: 0 }}
+                  animate={{ opacity: 1, y: 0, scale: 1 }}
+                  exit={{ opacity: 0, x: -100, scale: 0 }}
+                  transition={{
+                    delay: 1,
+                    duration: 1,
+                    type: "spring",
+                    bounce: 0.5,
+                    opacity: { ease: "linear" },
+                    layout: { duration: 0.3 },
+                  }}
+                  className="bg-white cursor-pointer p-3 shadow-xl relative col-span-3 text-left capitalize space-y-2 rounded-sm "
+                >
+                  <div className="relative overflow-hidden">
+                    <motion.img
+                      whileHover={{ scale: 1.2 }}
+                      initial={{ opacity: 0, x: -100, scale: 0 }}
+                      animate={{ opacity: 1, x: 0, scale: 1 }}
+                      transition={{
+                        duration: 0.7,
+                        bounce: 0.3,
+                        type: "spring",
+                      }}
+                      src={product}
+                      className="img z-10"
+                      alt=""
+                    />
+                  </div>
+                  <div>
+                    <motion.p
+                      initial={{ opacity: 0, y: 100 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      transition={{
+                        duration: 0.7,
+                        bounce: 0.3,
+                        type: "spring",
+                      }}
+                      className="inter text-lg"
+                    >
+                      {e.product}
+                    </motion.p>
+                    <motion.p
+                      initial={{ opacity: 0, y: 100 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      transition={{
+                        delay: 3.2,
+                        duration: 1,
+                        bounce: 0.3,
+                        type: "spring",
+                      }}
+                      className="text-sm"
+                    >
+                      {`Rp  ${e.harga},00`}
+                    </motion.p>
+                  </div>
+                  <motion.button
+                    whileHover={{ scale: 0.9 }}
+                    whileTap={{ scale: 1.1 }}
+                    className="text-center py-2 w-full bg-yellow-500 inter flex items-center justify-center space-x-5"
+                  >
+                    <p> Add To Cart</p> <BsFillCartPlusFill />
+                  </motion.button>
+                </motion.div>
+              ))
+          : data.map((e, i) => (
+              <motion.div
+                key={i}
+                layout
+                initial={{ opacity: 0, y: -100, scale: 0 }}
+                animate={{ opacity: 1, y: 0, scale: 1 }}
+                exit={{ opacity: 0, x: -100, scale: 0 }}
+                transition={{
+                  delay: 1,
+                  duration: 1,
+                  type: "spring",
+                  bounce: 0.5,
+                  opacity: { ease: "linear" },
+                  layout: { duration: 0.3 },
+                }}
+                className="bg-white cursor-pointer p-3 shadow-xl relative col-span-3 text-left capitalize space-y-2 rounded-sm "
+              >
+                <div className="relative overflow-hidden">
+                  <motion.img
+                    whileHover={{ scale: 1.2 }}
+                    initial={{ opacity: 0, x: -100, scale: 0 }}
+                    animate={{ opacity: 1, x: 0, scale: 1 }}
+                    transition={{
+                      duration: 0.7,
+                      bounce: 0.3,
+                      type: "spring",
+                    }}
+                    src={product}
+                    className="img z-10"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <motion.p
+                    initial={{ opacity: 0, y: 100 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{
+                      duration: 0.7,
+                      bounce: 0.3,
+                      type: "spring",
+                    }}
+                    className="inter text-lg"
+                  >
+                    {e.product}
+                  </motion.p>
+                  <motion.p
+                    initial={{ opacity: 0, y: 100 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{
+                      delay: 3.2,
+                      duration: 1,
+                      bounce: 0.3,
+                      type: "spring",
+                    }}
+                    className="text-sm"
+                  >
+                    {`Rp  ${e.harga},00`}
+                  </motion.p>
+                </div>
+                <motion.button
+                  whileHover={{ scale: 0.9 }}
+                  whileTap={{ scale: 1.1 }}
+                  className="text-center py-2 w-full bg-yellow-500 inter flex items-center justify-center space-x-5"
+                >
+                  <p> Add To Cart</p> <BsFillCartPlusFill />
+                </motion.button>
+              </motion.div>
+            ))}
+      </AnimatePresence>
     </div>
   );
 };
